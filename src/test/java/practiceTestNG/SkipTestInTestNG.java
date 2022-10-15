4900e0abf10856f8b9352f68b7ba3a8dedd246e8
NagKothapalli <nag022@gmail.com>
2022-10-15 10:08:15 +0530

    Oct15th code pushed

---
+++ src/test/java/practiceTestNG/SkipTestInTestNG.java
@@ -0,0 +1,34 @@
+package practiceTestNG;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class SkipTestInTestNG
+{
+	   @Test(priority=1,enabled=true)
+	   public void testcase1(){
+	      System.out.println("Testcase 1 - ");
+	   }
+	   @Test(priority=2 , enabled=true)
+	   public void testcase2(){
+		  System.out.println("Testcase 2 - ");
+	      System.out.println("Testcase 2 - skip exception example");
+	     // throw new SkipException("Skipping this exception");
+	      System.out.println("Executed Successfully");
+	   }
+	   @Test(priority=3 , enabled=true)
+	   public void testcase3(){
+		   System.out.println("Testcase 3 - ");
+		   //
+		   //
+		   //
+	      boolean DataAvailable=true;
+	      System.out.println("Test Case3 - Conditional Skip");
+	      if(!DataAvailable)
+	      {
+	    	  throw new SkipException("Skipping this exception"); 
+	      }	    	  
+	      System.out.println("Executed Successfully");
+	   }
+
+}
